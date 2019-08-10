a927764ce2e72b8690e3d44d168227135587a641
Lee Anderson <arrgeeveelee@gmail.com>
2019-08-10 10:29:59 +0800

    Added comments and made the method names less lazycache-y

--- src/MemoryCache.Testing.Moq/MockFactory.cs
+++ src/MemoryCache.Testing.Moq/MockFactory.cs
@@ -4,21 +4,34 @@ using Microsoft.Extensions.Logging;
 using Moq;
 
 namespace MemoryCache.Testing.Moq {
+    /// <summary>
+    /// Factory for creating mock/mocked instances.
+    /// </summary>
     public class MockFactory {
         private static readonly ILogger Logger = LoggerHelper.CreateLogger(typeof(MockFactory));
 
-        public static Mock<IMemoryCache> CreateCachingServiceMock() {
+        /// <summary>
+        /// Creates a memory cache mock.
+        /// </summary>
+        /// <returns>A memory cache mock.</returns>
+        public static Mock<IMemoryCache> CreateMemoryCacheMock() {
             var mock = new Mock<IMemoryCache>();
-
+            
             mock.Setup(m => m.CreateEntry(It.IsAny<object>()))
-                .Callback(() => Logger.LogDebug("Cache CreateEntry invoked"))
+                .Callback((object key) => {
+                    Logger.LogDebug("Cache CreateEntry invoked");
+                })
                 .Returns((object key) => new CacheEntryFake(key, mock));
             
             return mock;
         }
 
-        public static IMemoryCache CreateMockedCachingService() {
-            return CreateCachingServiceMock().Object;
+        /// <summary>
+        /// Creates a mocked memory cache.
+        /// </summary>
+        /// <returns>A mocked memory cache.</returns>
+        public static IMemoryCache CreateMockedMemoryCache() {
+            return CreateMemoryCacheMock().Object;
         }
     }
 }
